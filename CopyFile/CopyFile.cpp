1661ac5c50f374adce10ff32621e5944998a4f34
vladovinkin@book <vladovinkin@gmail.com>
2024-02-10 01:22:22 +0300

    Add project files.

---
+++ CopyFile/CopyFile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << argc << "\n";
+    for (int i = 0; i < argc; ++i)
+    {
+        std::cout << argv[i] << "\n";
+    }
+    return 0;
+}
