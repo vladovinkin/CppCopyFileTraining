90b26f0c87524922a39d694bca3ebd04734811ac
vladovinkin@book <vladovinkin@gmail.com>
2024-02-13 23:38:45 +0300

    Парсинг аргументов командной строки выделен в отдельную функцию

--- CopyFile/CopyFile.cpp
+++ CopyFile/CopyFile.cpp
@@ -3,10 +3,30 @@
 #include <string>
 #include <optional>
 
+struct Args
+{
+    std::string inputFileName;
+    std::string outputFileName;
+};
+
+std::optional<Args> ParseArgs(int argc, char* argv[])
+{
+    if (argc != 3)
+    {
+        return std::nullopt;
+    }
+    Args args;
+    args.inputFileName = argv[1];
+    args.outputFileName = argv[2];
+    return args;
+}
+
 int main(int argc, char* argv[])
 {
     // Проверка правильности аргументов командной строки
-    if (argc != 3)
+    auto args = ParseArgs(argc, argv);
+
+    if (!args)
     {
         std::cout << "Invalid arguments count\n";
         std::cout << "Usage: CopyFile.exe <input file name> <output file name>\n";
@@ -15,19 +35,19 @@ int main(int argc, char* argv[])
 
     // Открываем входной файл
     std::ifstream input;
-    input.open(argv[1]);
+    input.open(args->inputFileName);
     if (!input.is_open())
     {
-        std::cout << "Failed to open '" << argv[1] << "' for reading\n";
+        std::cout << "Failed to open '" << args->inputFileName << "' for reading\n";
         return 1;
     }
     
     // Открываем выходной файл
     std::ofstream output;
-    output.open(argv[2]);
+    output.open(args->outputFileName);
     if (!output.is_open())
     {
-        std::cout << "Failed to open '" << argv[2] << "' for writing\n";
+        std::cout << "Failed to open '" << args->outputFileName << "' for writing\n";
         return 1;
     }
 
